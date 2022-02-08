24584ae7d91a40e894822639e6d6a08e23dabd50
clanguo <1642805102@qq.com>
2022-02-08 16:53:55 +0800

    添加博客时请求tag和category

--- src/page/Blog/BlogAdd/index.tsx
+++ src/page/Blog/BlogAdd/index.tsx
@@ -1,13 +1,20 @@
 import { message } from 'antd';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import BlogForm, { TResetCallBack } from '../../../components/BlogForm';
 import { setLoadingAction } from '../../../redux/actions/AdminActions';
 import BlogActions from '../../../redux/actions/BlogActions';
+import CategoryActions from '../../../redux/actions/CategoryActions';
+import TagActions from '../../../redux/actions/TagActions';
 import BlogServices, { IBlog } from '../../../services/BlogServices';
 
 const BlogAdd: React.FC = () => {
 	const dispatch = useDispatch<any>();
+
+	useEffect(() => {
+		dispatch(TagActions.fetchTag());
+		dispatch(CategoryActions.fetchCategory());
+	});
 
 	const onSubmit = async (form: IBlog, callback: TResetCallBack) => {
 		dispatch(BlogActions.setLoadingAction(true));
